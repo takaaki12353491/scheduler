07235fd3cb1addc0f4754f43574d164e09bd2052
takaaki12353491 <takaaki12353491@gmail.com>
2021-01-15 11:23:08 +0900

    Refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react'
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux'
-import './App.css'
-import { ThemeProvider } from '@material-ui/core/styles'
-import theme from './styles/theme'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Month from './components/pages/month/Month'
-import rootReducer from './redux/rootReducer'
-
-const store = configureStore({reducer: rootReducer})
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Switch>
-            <Route component={Month}/>
-          </Switch>
-        </Router>
-      </ThemeProvider>
-    </Provider>
+    <Router>
+      <Switch>
+        <Route component={Month}/>
+      </Switch>
+    </Router>
   )
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import dayjs from "dayjs";
-import "dayjs/locale/ja";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './styles/theme'
+import rootReducer from './redux/rootReducer'
+import DayjsUtils from '@date-io/dayjs'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+import dayjs from 'dayjs'
+import 'dayjs/locale/ja'
 
-dayjs.locale("ja");
+dayjs.locale('ja')
+
+const store = configureStore({reducer: rootReducer})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <MuiPickersUtilsProvider utils={DayjsUtils}>
+          <App/>
+        </MuiPickersUtilsProvider>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
