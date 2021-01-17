9f3b618730034cdb084130515d13e8c106d64a6f
takaaki12353491 <takaaki12353491@gmail.com>
2021-01-18 07:52:53 +0900

    Move protoc.sh

--- protoc.sh
+++
@@ -1,9 +0,0 @@
-CLIENT_DIR=client/src/pb
-SERVER_DIR=server/pb
-
-protoc \
-  -I=proto \
-  --js_out=import_style=commonjs:${CLIENT_OUTPUT_DIR} \
-  --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_OUTPUT_DIR} \
-  --go_out=plugins=grpc:${SERVER_DIR} \
-  proto/*.proto
