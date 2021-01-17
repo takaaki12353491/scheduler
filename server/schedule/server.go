0bff987c0f95ba6ee9cf9b99addfc164f78dd704
takaaki12353491 <takaaki12353491@gmail.com>
2021-01-17 21:57:31 +0900

    Add comment

--- server/schedule/server.go
+++ server/schedule/server.go
@@ -10,21 +10,26 @@ type Server struct {
 }
 
 func (s *Server) Index(ctx context.Context, r *pb.IndexRequest) (*pb.IndexResponse, error) {
+	// TODO: Implement
 	return nil, nil
 }
 
 func (s *Server) Show(ctx context.Context, r *pb.ShowRequest) (*pb.IndexResponse, error) {
+	// TODO: Implement
 	return nil, nil
 }
 
 func (s *Server) Create(ctx context.Context, r *pb.CreateRequest) (*pb.CreateResponse, error) {
+	// TODO: Implement
 	return nil, nil
 }
 
 func (s *Server) Update(ctx context.Context, r *pb.CreateRequest) (*pb.CreateResponse, error) {
+	// TODO: Implement
 	return nil, nil
 }
 
 func (s *Server) Delete(ctx context.Context, r *pb.DeleteRequest) (*pb.DeleteResponse, error) {
+	// TODO: Implement
 	return nil, nil
 }
