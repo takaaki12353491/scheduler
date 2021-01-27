4c3e60a47982e5d40bc87d3556cbe6fabe83b2ea
takaaki12353491 <takaaki12353491@gmail.com>
2021-01-27 13:05:38 +0900

    Add Avator to Header

--- client/src/components/organisms/header/Auth.tsx
+++ client/src/components/organisms/header/Auth.tsx
@@ -1,21 +1,38 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import { Button, makeStyles } from '@material-ui/core'
+import { Button, Avatar, Menu, MenuItem, makeStyles } from '@material-ui/core'
 
 const Auth: React.FC = () => {
   const classes = useStyles()
   const { user, isAuthenticated, isLoading,loginWithRedirect, logout } = useAuth0()
+  const [ anchorEl, setAnchorEl ] = React.useState<null | HTMLElement>(null)
+  const RenderMenu = () => (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      keepMounted
+      transformOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      open={!!anchorEl}
+      onClose={() => setAnchorEl(null)}
+    >
+      <MenuItem onClick={() => logout()}>Log out</MenuItem>
+    </Menu>
+  )
   return (
     <div className={classes.auth}>
-      {!isLoading && isAuthenticated ?
-        <Button onClick={() => logout()}>
-          Log Out
-        </Button>
+      {isLoading ?
+      <div>Loading ...</div>
+      :
+      isAuthenticated ?
+        <Avatar src={user.picture} onClick={e => setAnchorEl(e.currentTarget)} className={classes.avator}>
+          {user.name}
+        </Avatar>
         :
         <Button onClick={() => loginWithRedirect()}>
           Log In
         </Button>
       }
+      <RenderMenu/>
     </div>
   )
 }
@@ -26,4 +43,7 @@ const useStyles = makeStyles(theme => ({
     position: 'absolute',
     right: '30px',
   },
+  avator: {
+    cursor: 'pointer',
+  }
 }))
