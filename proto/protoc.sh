8ba4721f15223bf4780c989504c03c5b14928091
takaaki12353491 <takaaki12353491@gmail.com>
2021-02-27 16:33:28 +0900

    Modify directory

--- proto/protoc.sh
+++ proto/protoc.sh
@@ -7,5 +7,11 @@ protoc \
   -I=${SCRIPT_DIR} \
   --js_out=import_style=commonjs:${CLIENT_DIR} \
   --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_DIR} \
-  --go_out=plugins=grpc:${API_DIR} \
-  ${SCRIPT_DIR}/*.proto
+
+for FILE in `ls *.proto`; do
+  API_DIR=${SCRIPT_DIR}/../server/api/${FILE%.*}
+  protoc \
+    -I=${SCRIPT_DIR} \
+    --go_out=plugins=grpc:${API_DIR}/app/pb \
+    ${SCRIPT_DIR}/${FILE}
+done
